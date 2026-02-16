0b9e230984504b55f21e27924dc6dd2f78ad0eb4
ialikh72 <ialikh.w1@gmail.com>
2026-02-16 19:02:54 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] arr = new int[5];
+        Console.WriteLine("INPUT NUMBERS");
+        for(int i=0;i <arr.Length;i++)
+        {
+            arr[i]= Convert.ToInt32(Console.ReadLine());
+        }
+        int start = arr[0];
+        for (int i=0; i<arr.Length; i++)
+        {
+            if (arr[i] != start + i)
+            {
+                Console.WriteLine("Missing number is: " + (start + i));
+                break;
+            }
+         
+        }
+
+    }
+}
